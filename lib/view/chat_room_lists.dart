962d671333da6942e04ae84f589127dd497bd423
solia0394@naver.com <15640527adb>
2023-10-24 19:23:36 +0900

    오류 주석처리

--- lib/view/chat_room_lists.dart
+++ lib/view/chat_room_lists.dart
@@ -15,124 +15,124 @@ class _ChatListsState extends State<ChatRoomLists> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-          .collection('ChatRooms')
-          .doc()
-          .collection('chatMessages')
-          .orderBy('sendingTime', descending: true)
-          .snapshots(),
-        builder: (context, snapshot) {
-          if(!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator(),);
-          }
-          final documents = snapshot.data!.docs;
-          print(documents);
-          return ListView(
-            children: documents.map((e) => _buildItemWidget(e)).toList(),
-          );
-        }
-      ),
+      // body: StreamBuilder<QuerySnapshot>(
+      //   stream: FirebaseFirestore.instance
+      //     .collection('ChatRooms')
+      //     // .doc()
+      //     // .collection('chatMessages')
+      //     // .orderBy('sendingTime', descending: true)
+      //     .snapshots(),
+      //   builder: (context, snapshot) {
+      //     if(!snapshot.hasData) {
+      //       return const Center(child: CircularProgressIndicator(),);
+      //     }
+      //     final documents = snapshot.data!.docs;
+      //     print(documents);
+      //     return ListView(
+      //       children: documents.map((e) => _buildItemWidget(e)).toList(),
+      //     );
+      //   }
+      // ),
     );
   }   // Widget build
 
-Future<Widget> _buildItemWidget(DocumentSnapshot doc) async{
-  // final chatRoomLists = ChatRoomsList(
-  //   contacter: doc['contacter'],
-  //   receiver: doc['receiver'],
-  //   chatMessages : doc['chatMessages']
-  // );
-  final chatRooms = ChatRooms(
-    // chatRoomId: doc.id, 
-    contacter: doc['contacter'], 
-    receiver: doc['receiver'], 
-    chatMessages: await getChatMessages(doc.id));
-  return Dismissible(
-    // direction: DismissDirection.endToStart,
-    // background: Container(
-    //   color: Colors.red,
-    //   alignment: Alignment.centerRight,
-    //   child: const Text("채팅방 나가기"),
-    // ),
-    key: ValueKey(doc),
-    onDismissed: (direction) {
-        // FirebaseFirestore.instance
-        // .collection('students')
-        // .doc(doc.id)
-        // .delete();
-      },     // 채팅방 나가기 (삭제)
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(3, 0, 3, 3),
-        child: Card(
-          child: ListTile(
-            contentPadding: const EdgeInsets.symmetric(horizontal: 0),
-            leading: const CircleAvatar(
-              backgroundImage: AssetImage(
-                'images/퍼그.png'
-              ),
-              radius: 50,
+// Future<Widget> _buildItemWidget(DocumentSnapshot doc) async{
+//   // final chatRoomLists = ChatRoomsList(
+//   //   contacter: doc['contacter'],
+//   //   receiver: doc['receiver'],
+//   //   chatMessages : doc['chatMessages']
+//   // );
+//   final chatRooms = ChatRooms(
+//     // chatRoomId: doc.id, 
+//     contacter: doc['contacter'], 
+//     receiver: doc['receiver'], 
+//     chatMessages: await getChatMessages(doc.id));
+//   return Dismissible(
+//     // direction: DismissDirection.endToStart,
+//     // background: Container(
+//     //   color: Colors.red,
+//     //   alignment: Alignment.centerRight,
+//     //   child: const Text("채팅방 나가기"),
+//     // ),
+//     key: ValueKey(doc),
+//     onDismissed: (direction) {
+//         // FirebaseFirestore.instance
+//         // .collection('students')
+//         // .doc(doc.id)
+//         // .delete();
+//       },     // 채팅방 나가기 (삭제)
+//       child: Padding(
+//         padding: const EdgeInsets.fromLTRB(3, 0, 3, 3),
+//         child: Card(
+//           child: ListTile(
+//             contentPadding: const EdgeInsets.symmetric(horizontal: 0),
+//             leading: const CircleAvatar(
+//               backgroundImage: AssetImage(
+//                 'images/퍼그.png'
+//               ),
+//               radius: 50,
               
-            ),
-            title: Row(
-              children: [
-                Text(
-                  chatRooms.contacter,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(0, 10, 15, 0),
-                    child: Text(
-                      "마지막 채팅 시간",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 13
-                      ),
-                    textAlign: TextAlign.right,
-                    ),
-                  ),
-                )
-              ],
-            ),
-            subtitle: Text(
-              "마지막 채팅 메시지",
-              style: TextStyle(
-                fontSize: 17
-              ),
-            ),
-          ),
-        ),
-      )
-    );
+//             ),
+//             title: Row(
+//               children: [
+//                 Text(
+//                   chatRooms.contacter,
+//                   style: const TextStyle(
+//                     fontWeight: FontWeight.bold,
+//                     fontSize: 20
+//                   ),
+//                 ),
+//                 Expanded(
+//                   child: Padding(
+//                     padding: EdgeInsets.fromLTRB(0, 10, 15, 0),
+//                     child: Text(
+//                       "마지막 채팅 시간",
+//                       style: TextStyle(
+//                         color: Colors.grey,
+//                         fontSize: 13
+//                       ),
+//                     textAlign: TextAlign.right,
+//                     ),
+//                   ),
+//                 )
+//               ],
+//             ),
+//             subtitle: Text(
+//               "마지막 채팅 메시지",
+//               style: TextStyle(
+//                 fontSize: 17
+//               ),
+//             ),
+//           ),
+//         ),
+//       )
+//     );
 
 
 
 
-  }
+//   }
 
-  // ------ functions -----
-  Future<ChatRooms> getChatMessages(String chatRoomId) async{
-    final chatMessages = await FirebaseFirestore.instance
-      .collection('chatMessages')
-      .snapshots();
-    // final chatMessages = <ChatMessages>[];
+//   // ------ functions -----
+//   Future<ChatRooms> getChatMessages(String chatRoomId) async{
+//     final chatMessages = await FirebaseFirestore.instance
+//       .collection('chatMessages')
+//       .snapshots();
+//     // final chatMessages = <ChatMessages>[];
 
-    final chats = ChatMessages(
-      content: content, 
-      sender: sender, 
-      sendingTime: sendingTime,
-    );
-    for (var messageDoc in chatMessagesQuery.docs) {
-      final messageData = messageDoc.data();
-      final chatMessage = ChatMessages.fromJson(messageData);
-      chatMessage.add(chatMessage);
-    }
+//     final chats = ChatMessages(
+//       content: content, 
+//       sender: sender, 
+//       sendingTime: sendingTime,
+//     );
+//     for (var messageDoc in chatMessagesQuery.docs) {
+//       final messageData = messageDoc.data();
+//       final chatMessage = ChatMessages.fromJson(messageData);
+//       chatMessage.add(chatMessage);
+//     }
 
-    return chatMessages;
-  }
+//     return chatMessages;
+//   }
 
 
 
