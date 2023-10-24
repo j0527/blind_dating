a98a01f334ed2d10ebc29d0e994cbdddf421ca49
seola <132555534+parkseola89@users.noreply.github.com>
2023-10-24 16:24:05 +0900

    yaml 수정, 카메라기능 구현

--- lib/view/signupsecond.dart
+++ lib/view/signupsecond.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:blind_dating/homewidget.dart';
 import 'package:blind_dating/view/signupthird.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUpSecond extends StatefulWidget {
   const SignUpSecond({super.key});
@@ -12,18 +15,19 @@ class SignUpSecond extends StatefulWidget {
 }
 
 class _SignUpSecondState extends State<SignUpSecond> {
-  late String inputValue;
+XFile? _image; //이미지를 담을 변수 선언
+  final ImagePicker picker = ImagePicker(); //ImagePicker 초기화
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
+  //이미지를 가져오는 함수
+  Future getImage(ImageSource imageSource) async {
+    //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
+    final XFile? pickedFile = await picker.pickImage(source: imageSource);
+    if (pickedFile != null) {
+      setState(() {
+        _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
+      });
+    }
   }
-
-
-
-  DateTime dateTime = DateTime(2000, 1, 1);
-
 
 
   @override
@@ -49,13 +53,14 @@ class _SignUpSecondState extends State<SignUpSecond> {
                   padding: const EdgeInsets.fromLTRB(50, 30, 50, 0),
                   child: Text('Sign Up', style: TextStyle(fontSize: 20, color: Color.fromRGBO(94, 88, 176, 0.945), fontWeight: FontWeight.bold),),
                 ),
-                SizedBox(height: 25,),
-                Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 0, 330, 0),
-                        child: Text('카메라', style: TextStyle(fontSize: 17, color: Color.fromARGB(255, 54, 54, 58),fontWeight: FontWeight.bold),),
-                          ),
+                SizedBox(height: 45, width: double.infinity),
+                      _buildPhotoArea(),
+                SizedBox(height: 55),
+                        _buildButton(),
+          
+                          
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(15, 25, 15, 50),
+                    padding: const EdgeInsets.fromLTRB(15, 75, 15, 80),
                     child: ElevatedButton(
                             onPressed: () {
                               Get.to(SignUpThird());
@@ -74,6 +79,59 @@ class _SignUpSecondState extends State<SignUpSecond> {
           ),
         ),
       ),
+    );
+  }
+
+
+  Widget _buildPhotoArea() {
+    return _image != null
+        ? Container(
+            width: 300,
+            height: 300,
+            child: Image.file(File(_image!.path)), //가져온 이미지를 화면에 띄워주는 코드
+          )
+        : Container(
+            width: 300,
+            height: 300,
+            color: Color.fromARGB(255, 212, 221, 247),
+          );
+  }
+
+
+  Widget _buildButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton.icon(
+          onPressed: () {
+            getImage(ImageSource.camera); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
+          },
+          style: TextButton.styleFrom(
+                        minimumSize: Size(130, 50),
+                          shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          ),
+                          backgroundColor: Color.fromARGB(255, 152, 175, 250),
+                          foregroundColor: Color.fromARGB(255, 234, 234, 236)
+                      ),
+          icon: Icon(Icons.camera_alt_outlined), 
+          label: Text("카메라", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)),
+          SizedBox(width: 30),
+        ElevatedButton.icon(
+          onPressed: () {
+            getImage(ImageSource.gallery); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
+          },
+          style: TextButton.styleFrom(
+                        minimumSize: Size(130, 50),
+                          shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          ),
+                          backgroundColor: Color.fromARGB(255, 152, 175, 250),
+                          foregroundColor: Color.fromARGB(255, 234, 234, 236)
+                      ),
+          icon: Icon(Icons.photo), 
+          label: Text("갤러리", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)),
+      ],
     );
   }
 }
