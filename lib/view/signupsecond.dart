d18f73c8d2dd2a33d868967aeb8ce1fad82e71d9
Rano-K <kmsoo0704@gmail.com>
2023-10-31 07:26:44 +0900

    signupsecond.dart수정
    너무많이 수정해서 뭐가 뭔지 모름

--- lib/view/signupsecond.dart
+++ lib/view/signupsecond.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:blind_dating/homewidget.dart';
 import 'package:blind_dating/view/signupthird.dart';
@@ -6,6 +7,76 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+
+//image를 flask로 보내는 함수
+// Future<String> uploadImage(XFile? imageFile) async {
+//   final Uri uri = Uri.parse('http://ec2-52-78-36-96.ap-northeast-2.compute.amazonaws.com:5000/upload');
+//   final http.MultipartFile file = await http.MultipartFile.fromPath('image', imageFile!.path);
+//   final http.Request request = (http.MultipartRequest('POST', uri)..files.add(file)) as http.Request;
+//   final http.StreamedResponse response = await request.send();
+//   if (response.statusCode == 200) {
+//     final Map<String, dynamic> responseBody = json.decode(await response.stream.bytesToString());
+//     return responseBody['results']; 
+//   } else {
+//     throw Exception('Failed to upload image');
+//   }
+// }
+//2차수정
+// Future<String> uploadImage(XFile? imageFile) async {
+//   if (imageFile == null) {
+//     throw Exception('No image provided');
+//   }
+  
+//   final Uri uri = Uri.parse('http://ec2-52-78-36-96.ap-northeast-2.compute.amazonaws.com:5000/upload');
+//   final http.MultipartFile file = await http.MultipartFile.fromPath('image', imageFile.path);
+
+//   final http.MultipartRequest request = http.MultipartRequest('POST', uri);
+//   request.files.add(file);
+
+//   final http.StreamedResponse response = await request.send();
+
+//   if (response.statusCode == 200) {
+//     final Map<String, dynamic> responseBody = json.decode(await response.stream.bytesToString());
+//     return responseBody['results']; 
+//   } else {
+//     throw Exception('Failed to upload image');
+//   }
+// }
+
+//3차수정
+Future<String> uploadImage(XFile? imageFile) async {
+  if (imageFile == null) {
+    throw Exception('No image provided');
+  }
+  
+  final Uri uri = Uri.parse('http://ec2-52-78-36-96.ap-northeast-2.compute.amazonaws.com:5000/upload');
+  final http.MultipartFile file = await http.MultipartFile.fromPath('image', imageFile.path);
+
+  final http.MultipartRequest request = http.MultipartRequest('POST', uri);
+  request.files.add(file);
+
+  final http.StreamedResponse response = await request.send();
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> responseBody = json.decode(await response.stream.bytesToString());
+    return responseBody['results']; 
+  } else {
+    throw Exception('Failed to upload image');
+  }
+}
+
+
+Stream<String> waitingTextStream() async* {
+  int count = 0;
+  while (true) {
+    await Future.delayed(Duration(milliseconds: 500));
+    count = (count + 1) % 4;  // 0, 1, 2, 3 순서로 반복
+    String dots = '.' * count;
+    yield '결과를 기다리는중$dots';
+  }
+}
 
 class SignUpSecond extends StatefulWidget {
   const SignUpSecond({Key? key}) : super(key: key);
@@ -14,12 +85,20 @@ class SignUpSecond extends StatefulWidget {
   State<SignUpSecond> createState() => _SignUpSecondState();
 }
 
+
+
+
 class _SignUpSecondState extends State<SignUpSecond> {
   XFile? _photoImage;
   XFile? _galleryImage;
+  bool hasReceivedResult = false; //모델결과값 받아오기 전까지는 다음버튼 비활성화
   final ImagePicker picker = ImagePicker();
+  String? dogTypeResult; // 강아지 종류를 저장하는 변수
+  bool isButtonPressed = false; // 버튼이 눌렸는지 여부
+
 
   Future getImage(ImageSource imageSource) async {
+    
     final XFile? pickedFile = await picker.pickImage(source: imageSource);
     if (pickedFile != null) {
       if (imageSource == ImageSource.camera) {
@@ -54,16 +133,89 @@ class _SignUpSecondState extends State<SignUpSecond> {
               "갤러리",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             )),
+            SizedBox(width: 20), // 간격을 위해 추가한 코드
+      ElevatedButton.icon(
+      onPressed: () async {
+    if (_galleryImage == null) {
+      // TODO: 사용자에게 이미지가 선택되지 않았음을 알리는 메시지 표시
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('에러'),
+          content: Text('이미지가 선택되지 않았습니다\n 이미지를 선택해서 "닮은 강아지 찾기"를 눌러주세요'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('확인'),
+            ),
+          ],
+        ),
+      );
+      return;
+    } else {
+      setState(() {
+        hasReceivedResult = false;
+        isButtonPressed = true; // 이미지가 선택되었을 때만 버튼이 눌렸다고 표시
+      });
+
+      try {
+        dogTypeResult = await uploadImage(_galleryImage); // 결과를 dogTypeResult에 저장
+
+        if (dogTypeResult != null) {
+          // 결과 팝업을 띄우는 코드
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('결과'),
+                content: Text('닮은 강아지 종류: $dogTypeResult'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('확인'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+
+        setState(() {
+          hasReceivedResult = true;
+        });
+      } catch (e) {
+        // TODO: 오류 메시지 표시
+      }
+    }
+},
+
+          style: TextButton.styleFrom(
+              minimumSize: Size(130, 50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              backgroundColor: Color.fromARGB(255, 152, 175, 250),
+              foregroundColor: Color.fromARGB(255, 234, 234, 236)),
+          icon: Icon(Icons.pets),
+          label: Text(
+            "닮은 강아지 찾기",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          )),
       ],
     );
   }
+
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
-          'Sign Up',
+          '       사진을 등록하세요\n 닮은 강아지를 찾아줄게요',
           style: TextStyle(
             fontSize: 20,
             color: Color.fromRGBO(94, 88, 176, 0.945),
@@ -86,20 +238,37 @@ class _SignUpSecondState extends State<SignUpSecond> {
             child: Column(
               children: [
                 Image.asset('images/stepsecond.png'),
+                // SizedBox(height: 35),
+                // _buildPhotoArea(),
                 SizedBox(height: 35),
-                _buildPhotoArea(),
-                SizedBox(height: 35),
-                _buildButton(),
+                // _buildButton(),
                 SizedBox(height: 25),
                 _buildGalleryArea(),
                 SizedBox(height: 25),
                 _buildGalleryButton(),
+                if (isButtonPressed && !hasReceivedResult) 
+                // Padding(
+                //   padding: const EdgeInsets.symmetric(vertical: 20),
+                //   child: Text('닮은 강아지: ${dogTypeResult ?? '결과를 기다리는 중...'}'),
+                // ),
+                StreamBuilder<String>(
+                  stream: waitingTextStream(),
+                  builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.hasData) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Text('닮은 강아지: ${dogTypeResult ?? snapshot.data}'),
+                      );
+                    }
+                    return SizedBox.shrink();  // 스트림에서 아직 데이터가 도착하지 않은 경우에 대한 위젯 (예: 로딩 인디케이터나 다른 위젯)
+                  },
+                ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(15, 75, 15, 80),
                   child: ElevatedButton(
-                    onPressed: () {
+                    onPressed: hasReceivedResult ? () {
                       Get.to(() => SignUpThird());
-                    },
+                    } : null,
                     style: ElevatedButton.styleFrom(
                       minimumSize: Size(400, 50),
                       backgroundColor: Color.fromARGB(255, 146, 148, 255),
@@ -140,40 +309,58 @@ class _SignUpSecondState extends State<SignUpSecond> {
   }
 
   Widget _buildGalleryArea() {
-    return _galleryImage != null
-        ? Container(
-            width: 300,
-            height: 300,
-            child: Image.file(File(_galleryImage!.path)),
-          )
-        : Container(
-            width: 300,
-            height: 300,
-            color: Color.fromARGB(255, 212, 221, 247),
-          );
-  }
-
-  Widget _buildButton() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton.icon(
+  return Stack(
+    children: [
+      _galleryImage != null
+          ? Container(
+              width: 300,
+              height: 300,
+              child: Image.file(File(_galleryImage!.path)),
+            )
+          : Container(
+              width: 300,
+              height: 300,
+              color: Color.fromARGB(255, 212, 221, 247),
+            ),
+      if (_galleryImage != null)
+        Positioned(
+          top: 5,
+          right: 5,
+          child: IconButton(
+            icon: Icon(Icons.close, color: Colors.red),
             onPressed: () {
-              getImage(ImageSource.camera);
+              setState(() {
+                _galleryImage = null;
+              });
             },
-            style: TextButton.styleFrom(
-                minimumSize: Size(130, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                backgroundColor: Color.fromARGB(255, 152, 175, 250),
-                foregroundColor: Color.fromARGB(255, 234, 234, 236)),
-            icon: Icon(Icons.photo),
-            label: Text(
-              "카메라",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            )),
-      ],
-    );
-  }
+          ),
+        ),
+    ],
+  );
+}
+
+
+  // Widget _buildButton() {
+  //   return Row(
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     children: [
+  //       ElevatedButton.icon(
+  //           onPressed: () {
+  //             getImage(ImageSource.camera);
+  //           },
+  //           style: TextButton.styleFrom(
+  //               minimumSize: Size(130, 50),
+  //               shape: RoundedRectangleBorder(
+  //                 borderRadius: BorderRadius.circular(10),
+  //               ),
+  //               backgroundColor: Color.fromARGB(255, 152, 175, 250),
+  //               foregroundColor: Color.fromARGB(255, 234, 234, 236)),
+  //           icon: Icon(Icons.photo),
+  //           label: Text(
+  //             "카메라",
+  //             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+  //           )),
+  //     ],
+  //   );
+  // }
 }
